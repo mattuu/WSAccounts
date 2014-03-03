afb44dc3bdf8a043afc0e1e7d6360bd7b95dfbed
mattuu <maciej@uliasz.com>
2014-03-03 22:41:41 +0000

    v0.4
    breeze/ko/webapi setup

---
+++ WS.Accounts.Site/ViewModels/home.js
@@ -0,0 +1,16 @@
+﻿define(["require", "exports"], function(require, exports) {
+    var homeViewModel = (function () {
+        function homeViewModel() {
+            var manager = new breeze.EntityManager('breeze/app');
+
+            var query = manager.EntityQuery().from("Accounts");
+
+            manager.executeQuery(query).then(function (d) {
+                return accounts(d);
+            });
+        }
+        return homeViewModel;
+    })();
+    exports.homeViewModel = homeViewModel;
+});
+//# sourceMappingURL=home.js.map
---
+++ WS.Accounts.Site/ViewModels/homeViewModel.js
@@ -0,0 +1,26 @@
+﻿var WS;
+(function (WS) {
+    /// <reference path="../Scripts/typings/jquery/jquery.d.ts" />
+    /// <reference path="../Scripts/typings/q/Q.d.ts" />
+    /// <reference path="../Scripts/typings/breeze/breeze.d.ts" />
+    /// <reference path="../Scripts/typings/knockout/knockout.d.ts" />
+    (function (Accounts) {
+        var homeViewModel = (function () {
+            function homeViewModel() {
+                var manager = new breeze.EntityManager('breeze/app');
+
+                var eq = new breeze.EntityQuery("Accounts");
+
+                manager.executeQuery(eq).then(function (d) {
+                    $.each(d, function (i, item) {
+                        this.accounts.push(item);
+                    });
+                });
+            }
+            return homeViewModel;
+        })();
+        Accounts.homeViewModel = homeViewModel;
+    })(WS.Accounts || (WS.Accounts = {}));
+    var Accounts = WS.Accounts;
+})(WS || (WS = {}));
+//# sourceMappingURL=homeViewModel.js.map
